9d865ca0c160bf1cc6ac899d02cf19e830f7a490
Daniil Boiko <daniel.boyko.al@gmail.com>
2019-10-05 21:51:17 +0300

    init

--- src/storage/SimpleLRU.h
+++ src/storage/SimpleLRU.h
@@ -17,7 +17,7 @@ namespace Backend {
  */
 class SimpleLRU : public Afina::Storage {
 public:
-    SimpleLRU(size_t max_size = 1024) : _max_size(max_size) {}
+    SimpleLRU(size_t max_size = 1024) : _max_size(max_size), _size_now(0) {}
 
     ~SimpleLRU() {
         _lru_index.clear();
