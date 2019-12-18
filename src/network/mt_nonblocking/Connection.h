f3647f843ba2de1fb220d92eae760f6e51b799b0
Daniil Boiko <daniel.boyko.al@gmail.com>
2019-12-18 14:56:53 +0300

    tabs

--- src/network/mt_nonblocking/Connection.h
+++ src/network/mt_nonblocking/Connection.h
@@ -15,54 +15,54 @@ namespace Afina {
 namespace Network {
 namespace MTnonblock {
 
-    class Connection {
-    public:
-        Connection(int s, std::shared_ptr<Afina::Storage> ps, std::shared_ptr<spdlog::logger> l) : _socket(s),
-                                                                                                   pStorage(ps), _logger(l) {
-            std::memset(&_event, 0, sizeof(struct epoll_event));
-            _event.data.ptr = this;
-        }
+class Connection {
+public:
+    Connection(int s, std::shared_ptr<Afina::Storage> ps, std::shared_ptr<spdlog::logger> l) : _socket(s),
+                                                                                               pStorage(ps), _logger(l) {
+        std::memset(&_event, 0, sizeof(struct epoll_event));
+        _event.data.ptr = this;
+    }
 
-        inline bool isAlive() const { return (_state == 0); }
+    inline bool isAlive() const { return (_state == 0); }
 
-        void Start();
+    void Start();
 
-    protected:
-        void OnError();
-        void OnClose();
-        void DoRead();
-        void DoWrite();
+protected:
+    void OnError();
+    void OnClose();
+    void DoRead();
+    void DoWrite();
 
-    private:
-        friend class Worker;
-        friend class ServerImpl;
+private:
+    friend class Worker;
+    friend class ServerImpl;
 
-        int _socket;
-        struct epoll_event _event;
+    int _socket;
+    struct epoll_event _event;
 
-        // 0 — alive
-        // 1 — error
-        // 2 — dead
-        int _state;
-        std::mutex mutex;
+    // 0 — alive
+    // 1 — error
+    // 2 — dead
+    int _state;
+    std::mutex mutex;
 
-        std::shared_ptr<spdlog::logger> _logger;
-        std::shared_ptr<Afina::Storage> pStorage;
-        std::shared_ptr<Afina::Logging::Service> pLogging;
+    std::shared_ptr<spdlog::logger> _logger;
+    std::shared_ptr<Afina::Storage> pStorage;
+    std::shared_ptr<Afina::Logging::Service> pLogging;
 
-        // Reading related
-        std::size_t arg_remains;
-        Protocol::Parser parser;
-        std::string argument_for_command;
-        std::unique_ptr<Execute::Command> command_to_execute;
+    // Reading related
+    std::size_t arg_remains;
+    Protocol::Parser parser;
+    std::string argument_for_command;
+    std::unique_ptr<Execute::Command> command_to_execute;
 
-        // Writing related
-        std::vector<std::string> _results;
-        int _written_amount;
+    // Writing related
+    std::vector<std::string> _results;
+    int _written_amount;
 
-        int already_read;
-        char client_buffer[4096];
-    };
+    int already_read;
+    char client_buffer[4096];
+};
 
 } // namespace MTnonblock
 } // namespace Network
